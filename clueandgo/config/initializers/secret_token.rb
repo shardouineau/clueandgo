1d93bd5dea3d4ae2a5fca08500aef0e2f6159abd
Simon Hardouineau <simon.hardouineau@hotmail.fr>
2013-10-11 14:32:40 +0200

    initial rails app

---
+++ clueandgo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clueandgo::Application.config.secret_token = '510f795144c09f0bd075fe45ea42fb25ab190343e29434d9cdc5610a609b9a0978c7c68e923f399d3863174c9b0c5f6527721e3faa30425f1abad1b98afd8a2f'
